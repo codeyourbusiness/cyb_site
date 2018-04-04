6d96225e78b9ddbec4a74aeb5d9d21514ea8bc98
nithinmk <sendtonthin@gmail.com>
2018-04-04 13:28:34 +0400

    added new host in settings

--- codeyourbusiness/settings/dev.py
+++ codeyourbusiness/settings/dev.py
@@ -8,7 +8,7 @@ SECRET_KEY = '2v2h^ose%h*)n!*$92(eswxg21*5sbl^iozw6#yvqw36j6-)-@'
 
 
 
-ALLOWED_HOSTS = ['codeyourbusiness.pythonanywhere.com', '127.0.0.1', 'cyb.eu-gb.mybluemix.net', '173.249.43.147',  ]
+ALLOWED_HOSTS = ['codeyourbusiness.pythonanywhere.com', '127.0.0.1', 'cyb.eu-gb.mybluemix.net', '173.249.43.147','codeyourbusiness',  ]
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
